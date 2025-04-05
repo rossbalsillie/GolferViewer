f92cb7d4b941ce275c7c6a2c5c37deb220f04b7f
Ross <rjb-dufc@hotmail.com>
2025-04-05 21:49:16 +0100

    Initial commit of plugin code

---
+++ Backend/GolferPlugins/GolferPlugins/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Text.Json;
+
+public class GetGolfersPlugin : IPlugin
+{
+    public void Execute(IServiceProvider serviceProvider)
+    {
+        var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+        var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+        var golfers = new[]
+        {
+            new { id = "1", firstName = "Rory", lastName = "McIlroy", nationality = "Northern Ireland" },
+            new { id = "2", firstName = "Scottie", lastName = "Scheffler", nationality = "USA" },
+            new { id = "3", firstName = "Jon", lastName = "Rahm", nationality = "Spain" },
+        };
+
+        var json = JsonSerializer.Serialize(golfers);
+        context.OutputParameters["GolferJson"] = json;
+    }
+}
---
+++ Backend/GolferPlugins/GolferPlugins/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GolferPlugins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GolferPlugins")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f18fa857-55c2-44fa-9d3c-22cda70038da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
